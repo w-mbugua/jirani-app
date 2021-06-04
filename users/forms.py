b6ff0c77a3c63589feffeba829b4d6e159c87ddb
JoyMbugua <wacumbugua@gmail.com>
2021-06-04 20:52:38 +0300

    updated profile views

--- users/forms.py
+++ users/forms.py
@@ -7,7 +7,7 @@ from posts.models import Post
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('neighborhood', 'location', 'occupation',)
+        fields = UserCreationForm.Meta.fields + ('email', 'neighborhood', 'location', 'occupation',)
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
