67b2ce2819b6b690f9ee65cee613b64653241e11
JoyMbugua <wacumbugua@gmail.com>
2021-06-04 11:29:05 +0300

    fixed registration bugs

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home
+from .views import home, SignUpView
 
 urlpatterns = [ 
     path('', home, name='home'),
+    path('signup/', SignUpView.as_view(), name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
 
 def home(request):
     return render(request, 'home.html')
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup/registration_form.html'
