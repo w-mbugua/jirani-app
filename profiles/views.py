0ea098ec9cb472476d82ce6479202942ae587fe4
JoyMbugua <wacumbugua@gmail.com>
2021-06-05 16:25:59 +0300

    fixed neighborhood/profile logic

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView, UpdateView
+from django.views.generic import DetailView, UpdateView, ListView, CreateView
 from .models import Profile
+from .forms import UserProfileForm
 
 class ProfileDetailView(DetailView):
     model = Profile
@@ -10,6 +11,22 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     fields = ('bio', 'photo')
     template_name = 'profiles/profile_edit.html'
+
+class ProfileListView(ListView):
+    model = Profile
+    context_object_name = 'profiles_list'
+    template_name = 'profiles/profiles_list.html'
+
+class ProfileCreateView(CreateView):
+    form_class = UserProfileForm
+    model = Profile
+    template_name = 'profiles/profile_reg.html'
+
+    def form_valid(self, form):
+        profile = form.save(commit=False)
+        profile.user = self.request.user
+        profile.save()
+        return super().form_valid(form)
 
 
 
