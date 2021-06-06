3d627eefdb33045809c0be191e6c0a32774a71d4
JoyMbugua <wacumbugua@gmail.com>
2021-06-06 14:24:32 +0300

    added post edit view

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
@@ -7,3 +8,12 @@ class PostDeleteView(DeleteView):
     model = Post
     template_name = 'posts/post_delete.html'
     success_url = reverse_lazy('home')
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'posts/update.html'
+    fields = ('body', 'image',)
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'posts/post_detail.html'
